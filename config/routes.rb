83fe1f396168c85926f00e63165c15e6e3a0f858
FumiakiTono <tonotorious@gmail.com>
2017-02-23 12:18:00 +0900

    create routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
+  resources :users, only: [:show, :edit, :update, :index, :new, :create]
+  resources :teachers, only: [:index, :show, :edit, :update, :new, :create]
+  resources :messages, only: [:index]
+  resources :posts, only: [:new, :create]
 end
