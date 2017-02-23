ee6e5bb9f2968282ed6a66fa94132ca2acd027e4
FumiakiTono <tonotorious@gmail.com>
2017-02-23 15:26:02 +0900

    Add validation on model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :image, content_type: ["image/jpg", "iamge/jpeg", "iamge/png"]
+  validates :name,     presence: true
+  validates :age,      presence: true
+  validates :email,    presence: true
+  validates :password, presence: true
 end
