b07c1de88376052e22181344ac71e596d1308f0c
FumiakiTono <tonotorious@gmail.com>
2017-03-01 12:02:40 +0900

    Create post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,18 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to new_post_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :image, :detail, :teacher_id)
   end
 
 end
