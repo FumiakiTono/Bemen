1a992d4f251c704d45be97cd95c28bd66567c658
FumiakiTono <tonotorious@gmail.com>
2017-03-01 20:38:03 +0900

    Change routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,12 @@ Rails.application.routes.draw do
     sessions:  "users/registrations"
   }
   root "users#index"
-  resources :users,     only: [:index, :show, :edit, :update]
-  resources :teachers,  only: [:index, :show, :edit, :update]
-  resources :messages,  only: [:index]
-  resources :posts,     only: [:new, :create] do
+  resources   :users,      only: [:index, :show, :edit, :update]
+  resources   :messages,   only: [:index]
+  resources   :teachers,   only: [:index, :show, :edit, :update] do
+    resources :messages,   only: [:new, :create]
+  end
+  resources   :posts,      only: [:new, :create] do
     collection do
       get "search"
     end
