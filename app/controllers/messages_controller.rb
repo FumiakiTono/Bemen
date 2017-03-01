ab3d61847f0c7e80e3539a630e1326c5dd2ae9c5
FumiakiTono <tonotorious@gmail.com>
2017-03-01 20:49:56 +0900

    Add some action on messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,24 @@
 class MessagesController < ApplicationController
 
   def index
+    @teachers = Teacher.all
+    @messages = Message.all.limit(5)
+  end
+
+  def new
+    @message = Message.new
+    @teacher = Teacher.find(2)
+  end
+
+  def create
+    @message = Message.new(message_post)
+    @message.save
+    redirect_to messages_path
+  end
+
+  private
+  def message_post
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, teacher_id: params[:teacher_id])
   end
 
 end
